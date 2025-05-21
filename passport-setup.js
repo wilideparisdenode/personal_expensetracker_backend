7b78e9afda9e879f5e939a230c015f605aa7bf24
Ndoping Wilson <w770106@gmail.com>
2025-05-21 12:50:22 +0100

    this is my nth+1 commit

--- passport-setup.js
+++ passport-setup.js
@@ -5,7 +5,7 @@ const User = require('./model/user'); // Adjust the path as necessary
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL:'http://localhost:5000/api/auth/google/callback', 
+  callbackURL:'https://personal-expensetracker-backend-673k.onrender.com/api/auth/google/callback', 
   proxy: true // Needed for HTTPS in production
 },
   async (accessToken, refreshToken, profile, done) => {
